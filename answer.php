137b6a9b5b811280506cc7dbe36741ee9ca1e0e8
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-28 18:38:03 +0000

    pleaseeeeeee

--- answer.php
+++ answer.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Watch a Movie, with PHP" />
+  <meta name="description" content="Ticket price for Museum, with PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-pink.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-deep_purple.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Watch a Movie, with PHP</title>
+  <title>Ticket price for Museum, with PHP</title>
 </head>
 
 <body>
@@ -22,31 +22,24 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Watch a Movie, with PHP</span>
+        <span class="mdl-layout-title">Ticket price for Museum, with PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/movie_ratings.jpg" alt="movie rating image" width="250" />
+        <img src="./images/student-discount.png" alt="Student discount image" width="250" />
       </div>
       <div class="page-content-answer">
         <div id="answer">
           <?php
           // input
           $userAge = $_GET["user-age"];
+          $day = $_GET["day"];
           // process
-          if ($userAge >= 17) {
-            // output
-            echo 'You can watch R movies alone.';
-          } else if ($userAge >= 13) {
-            // output
-            echo 'You can watch PG-13 movies alone.';
-          } else if ($userAge >= 5) {
-            // output
-            echo 'You can watch G or PG movies alone.';
+          if (($age >= 12 && $age <= 21) || $day == 'tuesday' || $day == 'thursday') {
+            echo 'You are eligible for the student pricing!';
           } else {
-            // output
-            echo 'You are too young for most of the movies.';
+            echo 'You must pay regular price.';
           }
           ?>
         </div>
--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Watch a Movie, with PHP" />
+  <meta name="description" content="Ticket price for Museum, with PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-pink.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-deep_purple.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Watch a Movie, with PHP</title>
+  <title>Ticket price for Museum, with PHP</title>
 </head>
 
 <body>
@@ -22,12 +22,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Watch a Movie, with PHP</span>
+        <span class="mdl-layout-title">Ticket price for Museum, with PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/movie_ratings.jpg" alt="movie rating image" width="250" />
+        <img src="./images/student-discount.png" alt="student discount image" width="250" />
       </div>
       <br />
       <div class="page-content-guide">Enter in your age</div>
@@ -37,6 +37,19 @@
             <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="user-age" />
             <label class="mdl-textfield__label" for="user-age">Your age</label>
             <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <br />
+          <div class="page-content-guide">Please select what day it is today</div>
+          <div class="mdl-textfield mdl-js-textfield">
+            <select class="mdl-textfield__input" name="day">
+              <option value="monday">Monday</option>
+              <option value="tuesday">Tuesday</option>
+              <option value="wednesday">Wednesday</option>
+              <option value="thursday">Thursday</option>
+              <option value="friday">Friday</option>
+              <option value="saturday">Saturday</option>
+              <option value="sunday">Sunday</option>
+            </select>
           </div>
           <br />
           <!-- Accent-colored raised button with ripple -->
